1e55400301be6bb3e090cb6ab2662634eb5c6138
schelen01 <88978043+schelen01@users.noreply.github.com>
2022-06-07 23:00:39 -0300

    Exercicio Doguinhos

---
+++ Aula06/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        adotaDog adotaDog = new adotaDog("Shakira", "true", "Dalmata", "2022", "5.1", "true", "sem ferimentos");
+
+    }
+}
---
+++ Aula06/src/adotaDog.java
@@ -0,0 +1,21 @@
+
+
+public class adotaDog {
+    private String nome;
+    private boolean adocao;
+    private String raca;
+    private int ano;
+    private double peso;
+    private boolean temChip;
+    private boolean ferido;
+
+    public AdotaDog(String shakira, String aTrue, String dalmata, String s, String s1, String aTrue1, String sem_ferimentos) {
+        this.nome = nome;
+        this.adocao = adocao;
+        this.raca = raca;
+        this.ano = ano;
+        this.temChip = temChip;
+        this.ferido = ferido;
+    }
+}
+
